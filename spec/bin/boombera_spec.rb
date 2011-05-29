0708e45f0d01cebcb1355efc0b1d79941704dfeb
John Wilger <johnwilger@gmail.com>
2011-05-28 21:59:14 -0700

    Added spec for updating existing content item

--- spec/bin/boombera_spec.rb
+++ spec/bin/boombera_spec.rb
@@ -3,12 +3,17 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 describe "The boombera CLI" do
   BOOMBERA_CLI = File.join(File.dirname(__FILE__), '..', '..', 'bin', 'boombera')
 
+  let(:db) { CouchRest.database!('http://127.0.0.1:5984/boombera_test') }
+
+  before(:each) do
+    db.delete!
+    db.create!
+  end
+
   describe "put command" do
     context "when putting a new content item via argument string" do
-      let(:db) { CouchRest.database!('http://127.0.0.1:5984/boombera_test') }
 
       before(:each) do
-        db.delete!
         @output = `#{BOOMBERA_CLI} put boombera_test /foo "some content"`
         @exit_status = $?.exitstatus
       end
@@ -21,12 +26,38 @@ describe "The boombera CLI" do
         @output.should == "Content Created: /foo\n"
       end
 
-      it 'stores the content in the couchdb server' do
+      it 'creates the content in the couchdb server' do
         result = db.documents['rows'].first
         result.should_not be_nil
         document = db.get(result['id'])
         document['path'].should == '/foo'
         document['body'].should == 'some content'
+      end
+    end
+
+    context "when updating an existing content item via argument string" do
+      before(:each) do
+        db.save_doc({:path => '/bar', :body => 'original content'})
+        @output = `#{BOOMBERA_CLI} put boombera_test /bar "new content"`
+        @exit_status = $?.exitstatus
+      end
+
+      it 'exits with a status code of 0' do
+        @exit_status.should == 0
+      end
+
+      it 'outputs a message indicating that the content was updated' do
+        @output.should == "Content Updated: /bar\n"
+      end
+
+      it 'updates the content in the couchdb server' do
+        rows = db.documents['rows']
+        rows.length.should == 1
+        result = rows.first
+        result.should_not be_nil
+        document = db.get(result['id'])
+        document['path'].should == '/bar'
+        document['body'].should == 'new content'
       end
     end
   end
